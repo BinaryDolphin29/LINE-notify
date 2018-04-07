006c20026a4d35c82987d384e4f23571f33971e7
BinaryDolphin29 <yuuyuu13961396@gmail.com>
2018-04-07 14:12:02 +0900

    修正

--- bot.js
+++ bot.js
@@ -1,11 +1,22 @@
 const request = require('request');
 
-request.post({
-	url: "https://notify-api.line.me/api/notify",
+// Token
+let Token = 'Y O U R  T O K E N'; // Token
+    Token = `Bearer ${Token}`;
+
+// Options
+const options = {
+	url: 'https://notify-api.line.me/api/notify',
 	method: 'POST',
+	json: true,
 	form: { message: 'メッセージ内容' },
 	headers: {
-		"Content-type": "application/json",
-		"Authorization": 'Bearer Token'
+		'Content-type': 'application/json',
+		'Authorization': Token
 	}
+}
+
+request(options, (err, res, body) => {
+	body = JSON.stringify(body, null, 2);
+	console.log(body);
 });
